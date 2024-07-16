c14d6b5cfc15bc381c8c8e09fadd24114ad3d66c
Vaibhav <v.vaibhavsingh@starhealth.in>
2024-07-17 02:26:54 +0530

    Demo of secondary constructor

---
+++ src/secondaryConstructor.kt
@@ -0,0 +1,13 @@
+fun main(){
+ val d1 = Data("data",)
+}
+
+class Data {
+    constructor(name:String){
+        val entityName = name
+    }
+
+    constructor(age:Number){
+        val entityAge= age
+    }
+}
