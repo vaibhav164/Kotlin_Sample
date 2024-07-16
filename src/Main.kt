611703fbc4252c4bc0b7864a5ea45254485d4058
Vaibhav <v.vaibhavsingh@starhealth.in>
2024-07-17 00:47:02 +0530

    Increment and decrememnt operator

--- src/Main.kt
+++ src/Main.kt
@@ -4,9 +4,11 @@ fun main() {
     var num1 = 5
     var num2 = 7
 
-    println(num1+num2)
-    println(num1-num2)
-    println(num2/num1)
-    println(num2%num1)
-    println(num1>num2)
+//    println(num1+num2)
+//    println(num1-num2)
+//    println(num2/num1)
+//    println(num2%num1)
+//    println(num1>num2)
+    println(num1++)
+    println(++num2)
 }
