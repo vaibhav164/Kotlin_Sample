0e06e218f75ff2677dee7491ea97363d3a997349
Vaibhav <v.vaibhavsingh@starhealth.in>
2024-07-17 02:36:33 +0530

    Demo for Geter and seter in Kotlin

---
+++ src/Geter_Seter.kt
@@ -0,0 +1,21 @@
+//Getter is used to get the value and setter is used to set the value
+fun main(){
+
+}
+class GetterSetterClass{
+    var name:String = "defaultValue"
+
+        get() = field
+
+        set(value){
+            field = value
+        }
+
+}
+
+/*the above code is equivalent to below one
+class GetterSetterClass {
+var name:String = "defaultValue"
+}
+
+* */
