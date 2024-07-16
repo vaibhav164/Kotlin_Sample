95bf4b550e19888841448a58732d61e578ed84dd
Vaibhav <v.vaibhavsingh@starhealth.in>
2024-07-17 02:07:49 +0530

    Regardeing the porperty of a class

--- src/constructorExample.kt
+++ src/constructorExample.kt
@@ -5,11 +5,14 @@ fun main(){
 }
 
 class User(name:String,age:Number, gender:String){
-
+//in above we can se the name , age and gender is created without using var or val
+//and these are not property User the property User are userName, userAge, userGender
        private var userName = name
         private var userAge = age
         private var userGender = gender
-
+    init{
+        println("${userName} is inside Init Block")
+    }
         fun showUserDetail(){
             println("${userName} age is ${userAge} and is ${userGender}")
         }
