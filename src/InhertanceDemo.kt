fa0c170d31a7d6bffc12d0f048161c1ce66920d5
vaibhav164 <vai1641996@gmail.com>
2024-07-17 11:17:30 +0530

    Looking into Inheritance in Kt

---
+++ src/InhertanceDemo.kt
@@ -0,0 +1,14 @@
+fun main(){
+
+}
+
+open class Vehicle(name:String, age:Int){
+    var Vehiclename = name
+    var VehicleAge= age
+}
+class Car1(name:String, age:Int):Vehicle(name, age){
+
+}
+class Bike(name:String, age:Int):Vehicle(name, age){
+
+}
