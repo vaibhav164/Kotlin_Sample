1af96bb679cd2a8b90de2ad04dd22e5814d8800c
vaibhav164 <vai1641996@gmail.com>
2024-07-18 03:09:31 +0530

    Visibility modifier in KT

---
+++ src/Visibility_Modifier.kt
@@ -0,0 +1,20 @@
+/*******
+ * visibility modifier refers to the accessibility of the declared entity in a program below are examples
+
+ public -> by default every entity is public
+ this entity will be visible to any client who can see the declaring class
+
+ private => visible inside the class only
+
+ protected->  visible inside the class and its derived classes(subclasses)
+
+ internal -> visible to any client inside the module that can see the declaring class
+
+ If we override the protected member in a derived class without specifying its visibility,  its visibility can also be
+ protected
+ *
+ *
+ *
+ *
+ *
+ * */
