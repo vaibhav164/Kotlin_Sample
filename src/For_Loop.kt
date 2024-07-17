54beb8e3e28cbbeeeff17da24ff5b406aa680d3b
vaibhav164 <vai1641996@gmail.com>
2024-07-18 03:36:14 +0530

    for loop in KT

---
+++ src/For_Loop.kt
@@ -0,0 +1,17 @@
+/****
+ * for in kotlin pretty looks like JS for loop lets see
+ * no it bit different below is a demo
+ */
+
+fun main(){
+
+//    for( i in 1..7){
+//        println(i)
+//    }
+
+    var text = "Vaibhav"
+
+    for(x in text){
+        println(x)
+    }
+}
