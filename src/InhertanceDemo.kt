1c342f731c6d1bb8e727c4f18fbbbd9ac0e95b50
vaibhav164 <vai1641996@gmail.com>
2024-07-18 02:46:21 +0530

    Looked the example of inheritance

--- src/InhertanceDemo.kt
+++ src/InhertanceDemo.kt
@@ -1,14 +1,30 @@
 fun main(){
-
+    val USER = Singer("Ansu", 67)
+    USER.userInfo();
 }
 
-open class Vehicle(name:String, age:Int){
-    var Vehiclename = name
-    var VehicleAge= age
+open class Human(name:String, age:Int) {
+    var name = name
+    var age = age
+    fun userInfo(){
+        println("${name} is ${age} years old")
+    }
 }
-class Car1(name:String, age:Int):Vehicle(name, age){
 
+class Singer (name:String, age:Int):Human(name, age){
+    var singerName = name
+    var singerAge = age
+    fun sings(){
+        println("${singerName} sings is ${singerAge} years old")
+    }
 }
-class Bike(name:String, age:Int):Vehicle(name, age){
 
+class MathTeacher(name:String, age:Int):Human(name,age){
+    var teacherName = name
+    var teacherAge = name
+
+    fun techesmaths(){
+        println("${teacherName} teaches maths and is ${teacherAge} years old")
+    }
 }
+
